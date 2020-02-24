cadd4c675cce6ddbc71c7747ca0cb5e5f5a3a7bf
Christopher Markieta <markietachristopher@gmail.com>
2020-02-24 12:27:29 -0500

    Adding single GCE example.

--- main.tf
+++ main.tf
@@ -3,3 +3,21 @@ provider "google" {
   project     = var.project
   region      = var.region
 }
+
+resource "google_compute_instance" "default" {
+  name         = var.vm_name
+  machine_type = var.vm_machine_type
+  zone         = var.vm_zone
+
+  tags = var.vm_tags
+
+  boot_disk {
+    initialize_params {
+      image = var.vm_boot_disk_image
+    }
+  }
+
+  network_interface {
+    network = var.vm_network_interface_network
+  }
+}
--- variables.tf
+++ variables.tf
@@ -12,3 +12,33 @@ variable "region" {
   default = "northamerica-northeast1"
   description = "GCP project region."
 }
+
+variable "vm_name" {
+  default = "vm1"
+  description = "VM instance name."
+}
+
+variable "vm_machine_type" {
+  default = "n1-standard-1"
+  description = "VM machine type."
+}
+
+variable "vm_zone" {
+  default = "northamerica-northeast1-c"
+  description = "GCP zone for VM to be created in."
+}
+
+variable "vm_tags" {
+  default = "dev"
+  description = "Tags to be attached to the VM instance."
+}
+
+variable "vm_boot_disk_image" {
+  default = "centos-cloud/centos-7"
+  description = "OS installation image."
+}
+
+variable "vm_network_interface_network" {
+  default = "default"
+  description = "Network name to be attached to VM instance's interface."
+}
